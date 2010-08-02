2551e5803ae717d83ff49f4e3bebad3db8569ea9
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2010-08-02 09:44:02 +0200

    Changed version to 0.5-SNAPSHOT in order to avoid confusion and also changed dependency to updated Jcommons library.

--- mds4-client/src/main/java/org/vpac/grisu/control/info/RankingAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package org.vpac.grisu.control.info;
-
-import au.org.arcs.jcommons.interfaces.GridResource;
-
-public interface RankingAlgorithm {
-	
-	public int getRank(GridResource gridResource);
-
-}
