c0292309a8673941da52f52eca15553b49744840
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-10-20 17:56:18 +1300

    Initial commit of Ynfo information manager library.

---
+++ ynfo/src/main/groovy/grisu/info/ynfo/YnfoManager.groovy
@@ -0,0 +1,62 @@
+package grisu.info.ynfo
+
+import grisu.jcommons.interfaces.InfoManager
+import grisu.jcommons.model.info.Directory
+
+class YnfoManager implements InfoManager {
+
+
+	static void main (args) {
+
+		def ym = new YnfoManager('/home/markus/src/infosystems/ynfo/src/test/resources/default_config.groovy')
+
+		for ( d in ym.getDirectoriesForVO('/nz/nesi') ) {
+			println d
+		}
+
+		for ( fs in ym.getFileSystemsForVO('/nz/nesi') ) {
+			println fs
+		}
+	}
+
+
+	def filesystems = []
+	def directories = []
+
+
+
+	public YnfoManager(String pathToConfig) {
+
+		ConfigObject config = new ConfigSlurper().parse(new File(pathToConfig).toURL())
+
+		for (def e in config) {
+
+			def name = e.key
+			def object = e.value
+			switch(object.class) {
+
+				case Directory.class:
+					directories.add(object)
+					object.alias = name
+					break
+				case FileSystem.class:
+					filesystems.add(object)
+				default: break
+			}
+		}
+	}
+
+
+	public Set<Directory> getDirectoriesForVO(String fqan) {
+
+		def fs = directories.findAll { it.fqan == fqan }
+		return fs as Set
+	}
+
+
+	public Set<FileSystem> getFileSystemsForVO(String fqan) {
+
+		def d = getDirectoriesForVO(fqan)
+		return d*.filesystem
+	}
+}
---
+++ ynfo/src/test/resources/default_config.groovy
@@ -0,0 +1,38 @@
+import grisu.jcommons.model.info.Directory
+import grisu.jcommons.model.info.FileSystem
+
+
+// filesystems
+auckland_gram5 = new FileSystem(
+	host:'gram5.ceres.auckland.ac.nz',
+	protocol:'gsiftp',
+	port:2811
+)
+
+canterbury_df = new FileSystem(
+			host:'df.canterbury.ac.nz'
+)
+
+
+// directories
+auckland_home = new Directory(
+	 	filesystem:auckland_gram5, 
+		fqan:"/nz/nesi", 
+		path:"/~/" 
+)
+
+auckland_tmp = new Directory(
+		filesystem:auckland_gram5,
+		fqan:"/nz/nesi",
+		path:"/tmp"
+)
+
+canterbury_home = new Directory(
+	filesystem:canterbury_df,
+		fqan:"/nz/nesi",
+		path:"/~/"
+)
+	
+
+
+
